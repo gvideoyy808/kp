e0b4f47bdd73f938e8f1638ae780be3c4cb6e8dc
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-04-13 22:39:31 +0800

    Add files via upload

---
+++ js/tj.js
@@ -0,0 +1,10 @@
+var m = 2;
+var n = Math.floor(Math.random() * m + 1);
+switch (n) {
+case 1:
+document.write('<scr'+'ipt src="//js.maidagu.com/c.aspx?action=c&c1=7&c2=603&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="></scr'+'ipt>');
+break;
+case 2:
+document.write('<scr'+'ipt src="//js.maidagu.com/c.aspx?action=c&c1=7&c2=603&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="></scr'+'ipt>');
+break;
+}
