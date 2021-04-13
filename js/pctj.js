a711c44849bb4c4d93dc72405d89ff38db28e28d
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-04-13 22:42:40 +0800

    Add files via upload

---
+++ js/pctj.js
@@ -0,0 +1,2 @@
+document.writeln("<script src=\'\/\/pc.weizhenwx.com\/pc\/rich-tf.js\' id=\"richid\" data=\'s=2903\'><\/script>");
+document.writeln("<script src=\'\/\/pc.weizhenwx.com\/pc\/beitou-tf.js\' id=\"beitouid\" data=\'s=2904\'><\/script>");
