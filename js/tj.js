19a631d956e7c74857721f7181d9345fd2fedc61
gvideoyy808 <gvideoyy@gmail.com>
2021-04-16 02:34:05 +0800

    .

--- js/tj.js
+++ js/tj.js
@@ -5,6 +5,11 @@ case 1:
 document.write('<scr'+'ipt src="//js.maidagu.com/c.aspx?action=c&c1=7&c2=603&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="></scr'+'ipt>');
 break;
 case 2:
-document.write('<scr'+'ipt src="//js.maidagu.com/c.aspx?action=c&c1=7&c2=603&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="></scr'+'ipt>');
+(function (){
+                        var id = "2415689770463175004-3956";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async defer src="https://www.happyyong.com/o.js"></script>');
+})();
 break;
 }
