e7089a3cce470bea1f21ff8dfba7d8df83a2e77d
gvideoyy808 <gvideoyy@gmail.com>
2021-04-20 17:24:46 +0800

    .

--- js/tj.js
+++ js/tj.js
@@ -2,14 +2,19 @@ var m = 2;
 var n = Math.floor(Math.random() * m + 1);
 switch (n) {
 case 1:
-document.write('<scr'+'ipt src="//js.maidagu.com/c.aspx?action=c&c1=7&c2=603&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="></scr'+'ipt>');
+(function (){
+                        var id = "283216112155948793-10030";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async  src="https://tj-yijiajing.com/o.js"></script>');
+})();
 break;
 case 2:
 (function (){
-                        var id = "2415689770463175004-3956";
+                        var id = "283216112155948793-10030";
   document.write('<ins style="display:none!important" id="' + id + '"></ins>');
   (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async defer src="https://www.happyyong.com/o.js"></script>');
+  document.write('<script async  src="https://tj-yijiajing.com/o.js"></script>');
 })();
 break;
 }
