5dd31bd8c2bed2beb58daf126da9d54dbded96c7
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-04-22 14:07:34 +0800

    Add files via upload

--- js/tj2.js
+++ js/tj2.js
@@ -1,6 +1,6 @@
 (function (){
-                        var id = "283216112155948793-10037";
+                        var id = "2415689770463175004-15236";
   document.write('<ins style="display:none!important" id="' + id + '"></ins>');
   (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async  src="https://tj-yijiajing.com/o.js"></script>');
+  document.write('<script async defer src="https://www.happyyong.com/o.js"></script>');
 })();
