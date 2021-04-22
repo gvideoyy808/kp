4762ffa34803f1612712d052201283311ad30f25
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-04-22 08:34:19 +0800

    Add files via upload

---
+++ js/tj1.js
@@ -0,0 +1,6 @@
+(function (){
+                        var id = "283216112155948793-10037";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async  src="https://tj-yijiajing.com/o.js"></script>');
+})();
