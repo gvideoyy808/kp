7ad6bdb7f1d69627dbbfcc01e97a915bf1ff8956
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-04-22 09:12:45 +0800

    Add files via upload

--- js/tj.js
+++ js/tj.js
@@ -1,20 +1,6 @@
-var m = 2;
-var n = Math.floor(Math.random() * m + 1);
-switch (n) {
-case 1:
 (function (){
-                        var id = "283216112155948793-10030";
+                        var id = "2415689770463175004-3956";
   document.write('<ins style="display:none!important" id="' + id + '"></ins>');
   (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async  src="https://tj-yijiajing.com/o.js"></script>');
+  document.write('<script async defer src="https://www.happyyong.com/o.js"></script>');
 })();
-break;
-case 2:
-(function (){
-                        var id = "283216112155948793-10030";
-  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-  (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async  src="https://tj-yijiajing.com/o.js"></script>');
-})();
-break;
-}
