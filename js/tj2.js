e0f8a9e936f43c4d94b8e7f313ce96132f4c206f
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-04-22 15:21:42 +0800

    Add files via upload

--- js/tj2.js
+++ js/tj2.js
@@ -1,6 +1,10 @@
-(function (){
-                        var id = "2415689770463175004-15236";
-  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-  (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async defer src="https://www.happyyong.com/o.js"></script>');
-})();
+var m = 2;
+var n = Math.floor(Math.random() * m + 1);
+switch (n) {
+case 1:
+
+break;
+case 2:
+
+break;
+}
