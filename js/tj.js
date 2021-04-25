c858a3d2a8c171e0dff9e7246957c7bf8462aa19
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-04-25 15:22:09 +0800

    Add files via upload

--- js/tj.js
+++ js/tj.js
@@ -1,6 +1,15 @@
+var m = 2;
+var n = Math.floor(Math.random() * m + 1);
+switch (n) {
+case 1:
+document.write('<scr'+'ipt src="//www.dianle.pw/c.aspx?action=c&c1=7&c2=141&c3=&c4=2&c5=AdCode_self&c6=640x200&c7=1&c8=1&c9=&c10="></scr'+'ipt>');
+break;
+case 2:
 (function (){
                         var id = "2415689770463175004-3956";
   document.write('<ins style="display:none!important" id="' + id + '"></ins>');
   (window.adbyunion = window.adbyunion || []).push(id);
   document.write('<script async defer src="https://www.happyyong.com/o.js"></script>');
 })();
+break;
+}
