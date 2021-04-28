040061672bdc8e733a6ea4276fb0761118cfb573
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-04-28 19:29:03 +0800

    Add files via upload

--- js/tj.js
+++ js/tj.js
@@ -2,19 +2,9 @@ var m = 2;
 var n = Math.floor(Math.random() * m + 1);
 switch (n) {
 case 1:
-(function (){
-                        var id = "2415689770463175004-3956";
-  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-  (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async defer src="https://www.happyyong.com/o.js"></script>');
-})();
+document.write('<scr'+'ipt async defer src="//www.abcdlm.com/c.aspx?action=c&c1=7&c2=125&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10=&c50=2402855" id="2402855"></scr'+'ipt>');
 break;
 case 2:
-(function (){
-                        var id = "2415689770463175004-3956";
-  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-  (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async defer src="https://www.happyyong.com/o.js"></script>');
-})();
+document.write('<scr'+'ipt async defer src="//www.abcdlm.com/c.aspx?action=c&c1=7&c2=125&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10=&c50=2402855" id="2402855"></scr'+'ipt>');
 break;
 }
