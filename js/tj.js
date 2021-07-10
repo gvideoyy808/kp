055c2b1ccc1c396e670448d882c7d3870fed1faf
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-07-10 12:42:54 +0800

    Add files via upload

--- js/tj.js
+++ js/tj.js
@@ -1,10 +1,1 @@
-var m = 2;
-var n = Math.floor(Math.random() * m + 1);
-switch (n) {
-case 1:
-document.write('<scr'+'ipt async defer src="//www.abcdlm.com/c.aspx?action=c&c1=7&c2=125&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10=&c50=2402855" id="2402855"></scr'+'ipt>');
-break;
-case 2:
-document.write('<scr'+'ipt async defer src="//www.abcdlm.com/c.aspx?action=c&c1=7&c2=125&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10=&c50=2402855" id="2402855"></scr'+'ipt>');
-break;
-}
+document.write('<scr'+'ipt async defer src="//www.abblm.com/c.aspx?action=c&c1=7&c2=125&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10=&c50=823158" id="823158"></scr'+'ipt>');
