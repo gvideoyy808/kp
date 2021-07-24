0b042de7996c3cc3eb2bcc1b74ec6238109e8e78
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-07-24 18:14:11 +0800

    Add files via upload

--- js/tj.js
+++ js/tj.js
@@ -1,1 +1,1 @@
-document.write('<scr'+'ipt async defer src="//www.abblm.com/c.aspx?action=c&c1=7&c2=125&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10=&c50=823158" id="823158"></scr'+'ipt>');
+document.write('<scr'+'ipt src="https://www.lm661.com/c.aspx?action=c&c1=7&c2=125&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="></scr'+'ipt>');
