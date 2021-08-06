ff98e56939b6fc7db27324b28208fe970a6ffc2d
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-08-06 21:06:44 +0800

    Add files via upload

---
+++ js/waptj.js
@@ -0,0 +1,1 @@
+document.write('<scr'+'ipt async defer src="https://www.lm661.com/c.aspx?action=c&c1=7&c2=125&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10=&c50=507171" id="507171"></scr'+'ipt>');
