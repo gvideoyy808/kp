f6cdea4e7a80a867248470aa83799f22b9ef82ff
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-08-06 20:46:00 +0800

    Add files via upload

--- js/tj.js
+++ js/tj.js
@@ -1,6 +1,1 @@
-(function (){
-                        var id = "2377029035902478992-10019";
-  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-  (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async  src="https://xsdj.lm663.com/o.js"></script>');
-})();
+document.write('<scr'+'ipt async defer src="https://www.lm661.com/c.aspx?action=c&c1=7&c2=125&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10=&c50=506616" id="506616"></scr'+'ipt>');
