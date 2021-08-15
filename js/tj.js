b7cb195f83a967dabf38185c5f4e13cb0582ee6c
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-08-15 19:46:14 +0800

    Add files via upload

--- js/tj.js
+++ js/tj.js
@@ -1,1 +1,8 @@
-
+document.writeln("<script>
+    (function () {
+        var id = \"2377029035902478992-10019\";
+        document.write(\'<ins style=\"display:none!important\" id=\"\' + id + \'\"><\/ins>\');
+        (window.adbyunion = window.adbyunion || []).push(id);
+    })();
+<\/script>
+<script async src=\"https:\/\/xsdj.lm663.com\/o.js\"><\/script>");
