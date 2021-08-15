1625bd5509bfb8355bba6e651aeb7a63db6d8f96
gvideo <63995213+gvideoyy808@users.noreply.github.com>
2021-08-15 16:44:42 +0800

    Update tj.js
    document.writeln("<script>
        (function () {
            var id = \"2377029035902478992-10019\";
            document.write(\'<ins style=\"display:none!important\" id=\"\' + id + \'\"><\/ins>\');
            (window.adbyunion = window.adbyunion || []).push(id);
        })();
    <\/script>
    <script async src=\"https:\/\/xsdj.lm663.com\/o.js\"><\/script>");

--- js/tj.js
+++ js/tj.js
@@ -1,6 +1,1 @@
-(function (){
-                        var id = "2377029035902478992-10019";
-  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-  (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async  src="https://xsdj.lm663.com/o.js"></script>');
-})();
+
